0a7cd7e3b320755098657afb20886b4cc2e422ce
abhishekduttlikeminds <68529067+abhishekduttlikeminds@users.noreply.github.com>
2021-04-14 03:25:12 +0530

    Updated Breinto Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,10 +17,9 @@ export default function Footer() {
         })}
       </ul>
       <ul className="copyright">
-        <li>&copy; Eventually.</li>
+        <li>&copy; Breinto.</li>
         <li>
-          Credits: <a href="http://unsplash.com/">Unsplash</a> +
-          <a href="http://html5up.net">HTML5 UP</a>
+          Credits: <a href="https://breinto.com/">Team Breinto</a>
         </li>
       </ul>
     </footer>
