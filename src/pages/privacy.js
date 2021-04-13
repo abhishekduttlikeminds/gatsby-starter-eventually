b71f4e9ed67817aacffdc7bc517d1dcf9235517b
abhishekdutt-blr <vikitripathi1blr@gmail.com>
2021-04-14 03:50:44 +0530

    Created Privacy Page

---
+++ src/pages/privacy.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import EmailForm from '../components/EmailForm';
+import SlideShow from '../components/SlideShow';
+
+var settings = {
+  images: [
+    { url: require('../assets/images/bg01.jpg'), position: 'center' },
+    { url: require('../assets/images/bg02.jpg'), position: 'center' },
+    { url: require('../assets/images/bg03.jpg'), position: 'center' },
+  ],
+  // Delay.
+  delay: 6000,
+};
+
+const PrivacyPage = () => (
+  <Layout>
+	<div>Hello world</div>
+    <SlideShow settings={settings} />
+  </Layout>
+);
+
+export default PrivacyPage;
